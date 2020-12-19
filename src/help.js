53215dda08d87576d25e684cbb7a8db7a2d54adc
pentill <76242873+pentill@users.noreply.github.com>
2020-12-20 05:19:41 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,35 @@
+const help = () => {
+	return '--------- *WhatsApp Bot *Y** ---------\n\n' +
+	'✧ Creator ✧\n' +
+	'❥ *#rtext [teks]*\n' +
+	'❥ *#stext [teks]*\n' +
+	'❥ *#nulis [teks]*\n' +
+	'❥ *#party [teks]*\n' +
+	'❥ *#phub [teks]*\n' +
+	'❥ *#bpink [teks]*\n' +
+	'❥ *#tahta [teks]*\n' +
+	'❥ *#tts [teks]*\n' +
+	'❥ *#qrcode [teks]*\n' +
+	'❥ *#thunder [teks]*\n' +
+	'❥ *#gtts [cc] [teks]*\n\n' +
+	'✧ Sticker Maker ✧\n' +
+	'❥ *#sticker*\n' +
+	'❥ *#gifsticker [captionVideo]*\n\n' +
+	'✧ Others Commands ✧\n' +
+	'❥ *#s [teks]* (SimSimi)\n' +
+	'❥ *#wiki [query]*\n' +
+	'❥ *#kbbi [query]*\n' +
+	'❥ *#chord [judul]*\n' +
+	'❥ *#cuaca [daerah]*\n' +
+	'❥ *#catfot*\n' +
+	'❥ *#meme*\n' +
+	'❥ *#pokemon*\n' +
+	'❥ *#toimg [reply]*\n\n' +
+	'✧ Owner Commands ✧\n' +
+	'❥ *#tagall*\n' +
+	'❥ *#gtitle [teks]*\n' +
+	'❥ *#gdesc [teks]*\n\n' +
+	'*Powered By Optimus J2 Prime*'
+}
+
+exports.help = help
