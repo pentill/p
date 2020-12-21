329cee82f29876434cb45ffcb1b1d44a24b32a99
pentill <76242873+pentill@users.noreply.github.com>
2020-12-21 19:04:43 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,48 @@
+const help = () => {
+	return '--------- *WhatsApp Bot *Y** ---------\n\n' +
+	'✧ Sticker Maker ✧\n' +
+	'❥ *#sticker*\n' +
+	'❥ *#toimg [reply]*\n' +
+	'❥ *#gifsticker [captionVideo]*\n\n' +
+	'✧ Education ✧\n' +
+	'❥ *#kbbi [query]*\n' +
+	'❥ *#brainly [query]*\n' +
+	'❥ *#wiki [query]* Wikipedia Indonesia\n' +
+	'❥ *#wikien [query]* Wikipedia English\n\n' +
+	'✧ Information ✧\n' +
+	'❥ *#ping*\n' +
+	'❥ *#covid*\n' +
+	'❥ *#covidcountry [negara]*\n' +
+	'❥ *#infocuaca [daerah]*\n' +
+	'❥ *#infoanime [teks]*\n' +
+	'❥ *#infoig [@username]*\n' +
+	'❥ *#jadwaltv [channel]*\n\n' +
+	'✧ Creator ✧\n' +
+	'❥ *#rtext [teks]*\n' +
+	'❥ *#stext [teks]*\n' +
+	'❥ *#nulis [teks]*\n' +
+	'❥ *#party [teks]*\n' +
+	'❥ *#phub [teks]*\n' +
+	'❥ *#bpink [teks]*\n' +
+	'❥ *#tahta [teks]*\n' +
+	'❥ *#tts [teks]*\n' +
+	'❥ *#qrcode [teks]*\n' +
+	'❥ *#thunder [teks]*\n' +
+	'❥ *#glitch [teks]*\n' +
+	'❥ *#gtts [cc] [teks]*\n\n' +
+	'✧ Others Commands ✧\n' +
+	'❥ *#s [teks]* (SimSimi)\n' +
+	'❥ *#chord [judul]*\n' +
+	'❥ *#lirik [judul]*\n' +
+	'❥ *#loli*\n' +
+	'❥ *#waifu*\n' +
+	'❥ *#nekonime*\n' +
+	'❥ *#catfot*\n' +
+	'❥ *#meme*\n' +
+	'❥ *#artinama [nama]*\n' +
+	'❥ *#pokemon*\n' +
+	'❥ *#indohot*\n' +
+	'*Powered By Optimus J2 Prime*'
+}
+
+exports.help = help
